1f1297b5263eb538766c4a3d5db84a1f025154e7
wayfar3 <daongocthang.vn@gmail.com>
2022-03-28 22:45:40 +0700

    update ad detection

--- js/content.js
+++ js/content.js
@@ -1,7 +1,13 @@
 $(()=>{
 	setInterval(()=>{
+
 		if($("button").hasClass("ytp-ad-skip-button")){
 			$("[class^='ytp-ad-skip-button']").trigger('click');			
-		}		
+		}
+
+		if($("button").hasClass("ytp-ad-overlay-close-button")){
+			$("[class^='ytp-ad-overlay-close-button']").trigger('click');
+		}
+		
 	},5000);
 });
